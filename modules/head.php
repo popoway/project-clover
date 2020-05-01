24ae59316ac7581d9ab600d67877be083b10a3fb
popoway <31576265+popoway@users.noreply.github.com>
2020-04-30 21:12:46 -0400

    feedUpload Escape String
    Attempt to fix #1

--- modules/head.php
+++ modules/head.php
@@ -30,10 +30,10 @@ if (!isset($page_title)) $page_title = "Untitled";
           <a class="nav-link" href="./?authuser=<?php echo $authuser; ?>&page=dates">Cool Dates</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://github.com/popoway/project-clover/wiki/TODO" target="_blank">TODO</a>
+          <a class="nav-link" href="https://github.com/popoway/project-clover/wiki/TODO" target="_blank">产品经理的需求</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="javascript:alert('没错，这个链接啥功能也木有 😛')">Very Very Suspicious 👀</a>
+          <a class="nav-link" href="https://github.com/popoway/project-clover/releases" target="_blank">发行日志</a>
         </li>
       </ul>
       <span class="navbar-text">
