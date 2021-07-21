2b84f44a0abe8b2d5cd57c5ed36430547959b1d3
Ming Lei <31576265+popoway@users.noreply.github.com>
2021-07-21 02:31:35 -0400

    feat: Composer and HTTPS
    Composer v2 and TLS 1.3 support

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if ($development_mode == "develop") {
   $site_url = "https://project-clover.app.popoway.cloud";
 }
 elseif ($development_mode == "production") {
-  $site_url = "http://clover.yeshan.ming.fyi";
+  $site_url = "https://clover.yeshan.ming.fyi";
 }
 
 # Define CDN URL
